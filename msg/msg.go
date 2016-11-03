40479a8feb07dbfa3a002183f2918c48e472d6f6
mofon <immofon@163.com>
2016-11-03 11:52:00 +0800

    add Type and interface implement vaild

--- msg/msg.go
+++ msg/msg.go
@@ -6,22 +6,43 @@ import (
 	"github.com/elemchat/elemchat/magic"
 )
 
+//go:generate stringer -type=Type
 type Type int
 
 const (
 	ILLEGAL Type = iota
+	INIT
+	WAIT_CHAT
+	WAIT_MAGIC
+	CHAT
+	MAGIC
+	EFFECT
+	DUALOVER
+)
+
+var (
+	_ Message = &Init{}
+	_ Message = &WaitChat{}
+	_ Message = &WaitMagic{}
+	_ Message = &Chat{}
+	_ Message = &Magic{}
+	_ Message = &Effect{}
+	_ Message = &Dualover{}
 )
 
 type Message interface {
 	Type() Type
 }
 
+type Init struct {
+}
+
 type WaitChat struct {
-	deadline time.Time
+	Deadline time.Time
 }
 
 type WaitMagic struct {
-	deadline time.Time
+	Deadline time.Time
 }
 
 type Chat struct {
@@ -30,4 +51,32 @@ type Chat struct {
 
 type Magic struct {
 	Magic magic.Magic
+}
+
+type Effect struct {
+}
+
+type Dualover struct {
+}
+
+func (_ *Init) Type() Type {
+	return INIT
+}
+func (_ *WaitChat) Type() Type {
+	return WAIT_CHAT
+}
+func (_ *WaitMagic) Type() Type {
+	return WAIT_MAGIC
+}
+func (_ *Chat) Type() Type {
+	return CHAT
+}
+func (_ *Magic) Type() Type {
+	return MAGIC
+}
+func (_ *Effect) Type() Type {
+	return EFFECT
+}
+func (_ *Dualover) Type() Type {
+	return DUALOVER
 }
