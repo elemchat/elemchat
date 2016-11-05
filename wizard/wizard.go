d6334e1e18a62ba66e6e803d1447fa2000b2642e
mofon <immofon@163.com>
2016-11-05 09:01:29 +0800

    add Wizard.Send() .Close() New()

---
+++ wizard/wizard.go
@@ -0,0 +1,134 @@
+package wizard
+
+import (
+	"time"
+
+	"github.com/elemchat/elemchat/codec"
+	"github.com/elemchat/elemchat/conn"
+	"github.com/elemchat/elemchat/msg"
+)
+
+type AttrValue int
+type Attr struct {
+	Blood AttrValue
+}
+
+type Message interface {
+	Wizard() *Wizard
+	Time() time.Time
+	Msg() msg.Message
+}
+type message struct {
+	wizard *Wizard
+	time   time.Time
+	msg    msg.Message
+}
+
+func (m *message) Wizard() *Wizard {
+	return m.wizard
+}
+func (m *message) Time() time.Time {
+	return m.time
+}
+func (m *message) Msg() msg.Message {
+	return m.msg
+}
+
+type Wizard struct {
+	conn  conn.Conn
+	codec codec.Codec
+	recv  chan<- Message // closed by other,not Wizard
+
+	Name string
+
+	Attr Attr
+}
+
+// Wizard do NOT close chan recv
+func New(name string, attr Attr,
+	conn conn.Conn, codec codec.Codec, recv chan<- Message) *Wizard {
+	w := &Wizard{
+		conn:  conn,
+		codec: codec,
+		recv:  recv,
+		Name:  name,
+		Attr:  attr,
+	}
+
+	go w.recvLoop()
+	return w
+}
+
+func (w *Wizard) Closed() bool {
+	if w.conn == nil {
+		return true
+	}
+	return false
+}
+func (w *Wizard) close() {
+	if w.conn != nil {
+		w.conn.Close()
+		w.conn = nil
+	}
+}
+
+func (w *Wizard) Send(msg msg.Message) bool {
+	if w.Closed() {
+		return false
+	}
+	// encode
+	if w.codec == nil {
+		w.close()
+		return false
+	}
+	data, err := w.codec.Encode(msg)
+	if err != nil {
+		return false
+	}
+
+	// write
+	err = w.conn.Write(data)
+	if err != nil {
+		// handle err
+		if err == conn.ErrClosed {
+			w.close()
+		}
+
+		return false
+	}
+	return true
+}
+
+func (w *Wizard) recvLoop() {
+	for {
+		// read
+		if w.conn == nil {
+			return
+		}
+		data, err := w.conn.Read()
+		if err != nil {
+			// handle err
+			if err == conn.ErrClosed {
+				w.close()
+			}
+		}
+
+		// decode
+		if w.codec == nil {
+			w.close()
+			return
+		}
+		msg, err := w.codec.Decode(data)
+		if err != nil {
+			// handle err
+			continue
+		}
+
+		// send to recv chan
+		w.recv <- &message{
+			wizard: w,
+			time:   time.Now(),
+			msg:    msg,
+		}
+	}
+}
---
+++ wizard/wizard_test.go
@@ -0,0 +1,117 @@
+package wizard
+
+import (
+	"reflect"
+	"sync"
+	"testing"
+
+	"github.com/elemchat/elemchat/codec"
+	"github.com/elemchat/elemchat/conn"
+	"github.com/elemchat/elemchat/msg"
+)
+
+func TestNew_Send(t *testing.T) {
+	s, c := conn.TestPair()
+	defer s.Close()
+	defer c.Close()
+
+	recv := make(chan Message)
+	w := New("wizard", Attr{Blood: 10}, c, codec.JsonCodec(), recv)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		data, err := s.Read()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		m, err := codec.JsonCodec().Decode(data)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if m == nil {
+			t.Error("expect not nil got nil")
+			return
+		}
+		if m, ok := m.(*msg.Chat); !ok {
+			t.Error("expect *msg.Chat got", reflect.TypeOf(m))
+			return
+		} else {
+			if m.Text != "hello wizard!" {
+				t.Error("expect hello wizard! got", m.Text)
+				return
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		w.Send(&msg.Chat{Text: "hello wizard!"})
+	}()
+	wg.Wait()
+}
+
+func TestNew_recvLoop(t *testing.T) {
+	s, c := conn.TestPair()
+	defer s.Close()
+	defer c.Close()
+
+	recv := make(chan Message)
+	w := New("wizard", Attr{Blood: 10}, c, codec.JsonCodec(), recv)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	// recv
+	go func() {
+		defer wg.Done()
+
+		m := <-recv
+		if m == nil {
+			t.Error("expect not nil got nil")
+			return
+		}
+		if m.Wizard() != w {
+			t.Error("expect", w, "got", m.Wizard())
+			return
+		}
+
+		if m.Msg() == nil {
+			t.Error("expect not nil got nil")
+			return
+		}
+
+		if m, ok := m.Msg().(*msg.Chat); !ok {
+			t.Error("expect *msg.Chat got", reflect.TypeOf(m))
+			return
+		} else {
+			if m.Text != "hello recvLoop!" {
+				t.Error("expect hello recvLoop! got", m.Text)
+				return
+			}
+		}
+	}()
+	// send
+	go func() {
+		defer wg.Done()
+
+		data, err := codec.JsonCodec().Encode(&msg.Chat{Text: "hello recvLoop!"})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		err = s.Write(data)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+	}()
+	wg.Wait()
+	close(recv)
+}
